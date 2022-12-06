e39a956b5691328f05a5d6607217094f30bb8947
Abhinav <abhinavgautam25@gmail.com>
2022-12-06 19:51:31 +0530

    Delete main directory

--- main/java/com/junit5mockito/exception/ContactNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.junit5mockito.exception;
-
-public class ContactNotFoundException extends RuntimeException {
-    public ContactNotFoundException(String message) {
-        super(message);
-    }
-}
