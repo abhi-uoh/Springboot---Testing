15ba0c79561f0265ceec8e11b1ec8538d7eb2181
Abhinav <abhinavgautam25@gmail.com>
2022-12-06 19:51:57 +0530

    Delete test directory

--- test/java/com/junit5mockito/TestH2Repository.java
+++
@@ -1,7 +0,0 @@
-package com.junit5mockito;
-
-import com.junit5mockito.entity.Contact;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TestH2Repository extends JpaRepository<Contact,Long> {
-}
--- test/java/com/junit5mockito/repository/ContactRepoTest.java
+++
@@ -1,9 +0,0 @@
-package com.junit5mockito.repository;
-
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-
-@DataJpaTest
-public class ContactRepoTest {
-
-
-}
